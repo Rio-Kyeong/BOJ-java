48495592f3b96cf237b6200b30002dc752e1708d
KyeongWoo Ryu <80517285+RyuKyeongWoo@users.noreply.github.com>
2021-05-19 18:30:54 +0900

    Add files via upload

---
+++ ifmun/joon2884.java
@@ -0,0 +1,29 @@
+package ifmun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class joon2884 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		
+		int H = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+	
+		if(M>=45) {
+			M = M-45;
+			System.out.println(H+" "+M);
+		}else if(M<45) {
+			H--;
+			M = 60-(45-M);
+			if(H < 0) {
+				H = 23;
+			}
+			System.out.println(H+" "+M);	
+		}
+		br.close();
+	}
+}
