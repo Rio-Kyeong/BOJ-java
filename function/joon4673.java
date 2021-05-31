d90cefafa3e47c3f1469909b8147c2901f4458ee
KyeongWoo Ryu <80517285+RyuKyeongWoo@users.noreply.github.com>
2021-05-31 18:04:51 +0900

    Func

---
+++ function/joon4673.java
@@ -0,0 +1,38 @@
+package function;
+
+public class joon4673 {
+	
+	public static void main(String[] args) {
+		boolean[] flag = new boolean[10001];
+		
+		for(int i = 1; i <= 10000; i++) {
+			int num = d(i);
+			
+			if(num <= 10000) {
+				flag[num]= true;
+			}
+			
+		}
+		
+		for(int i = 1; i <= 10000; i++) {
+			if(!flag[i]) {
+				System.out.println(i);
+			}
+		}
+}
+
+	public static int d(int number) {
+		
+		int sum = number; //1234 = 1+2+3+4+1234 = 1244
+		
+		while(number != 0) {
+			sum = sum + (number%10);
+			number = number/10;
+		}
+		
+		return sum;
+	}
+		
+
+
+}
