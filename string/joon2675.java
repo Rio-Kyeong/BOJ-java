4cf7704386cb448b7b96c3bf025f14f6223bedce
KyeongWoo Ryu <80517285+RyuKyeongWoo@users.noreply.github.com>
2021-07-05 20:49:52 +0900

    Add files via upload

---
+++ string/joon2675.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon2675 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt( br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < t; i++) {
+			String input = br.readLine();			
+			
+			StringTokenizer st = new StringTokenizer(input," ");
+			
+			int r = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+			
+			for(int p = 0; p < s.length(); p++) {
+				for(int j = 0; j < r; j++) {
+					sb.append(s.charAt(p));
+				}
+			}
+			sb.append("\n");
+		}	
+		System.out.println(sb.toString());
+	}
+}
