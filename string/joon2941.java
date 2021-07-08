0b4a3635cec6e0332a8b8bc583eb32133c40468e
KyeongWoo Ryu <80517285+RyuKyeongWoo@users.noreply.github.com>
2021-07-08 14:26:17 +0900

    Add files via upload

---
+++ string/joon2941.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+
+public class joon2941 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine().trim();
+		String[] arrCon = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+		
+		for(int i = 0; i < arrCon.length; i++) {
+			if(input.contains(arrCon[i])){
+				input = input.replaceAll(arrCon[i]," ");
+			}
+		}
+		
+		System.out.println(input.length());
+		
+		}
+	}
+
+
