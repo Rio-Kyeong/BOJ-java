e9383f8b824a28cd60f35972fdde696eb355432d
KyeongWoo Ryu <80517285+RyuKyeongWoo@users.noreply.github.com>
2021-07-10 16:24:58 +0900

    Add files via upload

---
+++ math1/joon2292.java
@@ -0,0 +1,32 @@
+package math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class joon2292 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int result = 1;
+		int cnt = 1;
+		int sum = 6;
+		
+		while(true) {
+			if( n <= cnt) {
+				break;
+			}
+			cnt += sum;
+			sum += 6;
+			result++;
+		}
+		
+		System.out.println(result);
+		
+		
+	}
+
+}
