ccaf5949fd6a3b798be1b7fb9601e5c2f49741fe
KyeongWoo Ryu <drd9811@naver.com>
2021-10-23 14:21:37 +0900

    Add files via upload

---
+++ math1/joon2839_2.java
@@ -0,0 +1,35 @@
+package math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 설탕 배달
+ */
+public class joon2839_2 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        while (true){
+            if(n % 5 == 0){
+               cnt += n / 5;
+               System.out.println(cnt);
+               break;
+            }else{
+               n -= 3;
+               cnt++;
+            }
+            if(n < 0){ // 4, 7
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
