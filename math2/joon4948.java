6ad556509b0e289f5c6dba4072588807445e3529
KyeongWoo Ryu <drd9811@naver.com>
2021-10-27 16:29:33 +0900

    Add files via upload

---
+++ math2/joon4948.java
@@ -0,0 +1,51 @@
+package math2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 베르트랑 공준
+ */
+public class joon4948 {
+
+    public static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+
+            int n = Integer.parseInt(br.readLine());
+            int cnt = 0;
+
+            if(n == 0){
+                break;
+            }
+
+            prime = new boolean[2*n+1];
+            get_prime();
+
+            for(int i = n+1; i <= 2*n; i++){
+                if(!prime[i]){
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+
+    public static void get_prime(){
+        // false = 소수
+        prime[0] = prime[1] = true;
+
+        for(int i =2; i < Math.sqrt(prime.length); i++){
+            if(prime[i]) continue;
+
+            for(int j = i*i; j < prime.length; j+=i){
+                prime[j] = true;
+            }
+        }
+    }
+}
