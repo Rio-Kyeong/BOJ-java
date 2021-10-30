5aea9c7a9e51f7535f36f99d77f420b15865fa49
KyeongWoo Ryu <drd9811@naver.com>
2021-10-30 16:50:56 +0900

    Add files via upload

---
+++ math2/joon1085.java
@@ -0,0 +1,29 @@
+package math2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 직사각형에서 탈출
+ */
+public class joon1085 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min_x = Math.min(x, w - x);
+        int min_y = Math.min(y, h-y);
+        int min = Math.min(min_x, min_y);
+
+        System.out.println(min);
+    }
+}
