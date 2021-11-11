0b87ee8af9c429e5772b76f31ae96be69bb19b18
KyeongWoo Ryu <drd9811@naver.com>
2021-11-11 17:26:40 +0900

    Update Joon2231.java

--- bruteForce/Joon2231.java
+++ bruteForce/Joon2231.java
@@ -12,7 +12,6 @@ public class Joon2231 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         int result = 0;
