096245f8c6b7d95419893a1bc6b0a716d23ba5ed
KyeongWoo Ryu <drd9811@naver.com>
2021-12-23 15:00:20 +0900

    update 211223

---
+++ bruteForce/Joon1436.java
@@ -0,0 +1,28 @@
+package bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 영화감독 숌
+ */
+public class Joon1436 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int num = 666;
+        int count = 1;
+
+        while (count != n) {
+            num++;
+
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
