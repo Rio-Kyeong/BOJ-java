60091292aeca4096cfcf5363c9dfd5dead85105a
KyeongWoo Ryu <drd9811@naver.com>
2022-02-16 18:28:12 +0900

    Add files via upload

---
+++ inputOutput/Joon10718.java
@@ -0,0 +1,12 @@
+package inputOutput;
+
+/**
+ * We love kriii
+ */
+public class Joon10718 {
+
+    public static void main(String[] args) {
+        System.out.print("강한친구 대한육군\n");
+        System.out.print("강한친구 대한육군");
+    }
+}
