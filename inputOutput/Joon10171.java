bab206db343349cfd19e556c92fb4430933c2556
KyeongWoo Ryu <drd9811@naver.com>
2022-02-20 17:59:21 +0900

    Add files via upload

---
+++ inputOutput/Joon10171.java
@@ -0,0 +1,14 @@
+package inputOutput;
+
+/**
+ * 고양이
+ */
+public class Joon10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
