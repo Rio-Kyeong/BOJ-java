8efcd900e7cacc5222fbdf6e5abe54386efb3f07
KyeongWoo Ryu <drd9811@naver.com>
2022-02-27 13:36:56 +0900

    Add files via upload

---
+++ inputOutput/Joon10926.java
@@ -0,0 +1,16 @@
+package inputOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * ??!
+ */
+public class Joon10926 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine() + "??!");
+    }
+}
